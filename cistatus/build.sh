08c27acb0793d85925888e4e235c377824153f80
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-10-08 10:00:05 +0200

    Oppdatert til ny klient

--- cistatus/build.sh
+++ cistatus/build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 fyne-cross --targets=linux/amd64 .
-mv build/main-linux-amd64 cistatus
+mv build/ciStatus-linux-amd64 cistatus
 fyne package -os linux -icon icon.png
 tar -zcvf linux.tar.gz usr
 rm -rf usr
 mv linux.tar.gz release/.
 
 fyne-cross --targets=windows/amd64 .
-mv build/main-windows-amd64.exe cistatus.exe
+mv build/ciStatus-windows-amd64.exe cistatus.exe
 fyne package -os windows -icon icon.png
 fyne-cross --targets=windows/amd64 .
-mv build/main-windows-amd64.exe cistatus.exe
+mv build/ciStatus-windows-amd64.exe cistatus.exe
 fyne package -os windows -icon icon.png
 mv cistatus.exe release/.
 
 fyne-cross --targets=darwin/amd64 .
-mv build/main-darwin-amd64 cistatus
+mv build/ciStatus-darwin-amd64 cistatus
 fyne package -os darwin -icon icon.png
 rm -rf release/cistatus.app
 mv cistatus.app release/.
--- cistatus/main.go
+++ cistatus/main.go
@@ -2,71 +2,249 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
-	"github.com/jszwedko/go-circleci"
-	"io/ioutil"
 	"log"
 	"net/url"
-	"os"
 	"time"
 )
 
-type App struct {
-	projects []*circleci.Project
-	a        fyne.App
-	w        fyne.Window
-	modal    *widget.PopUp
+type ciStatusLayout struct {
+	canvas   fyne.CanvasObject
+	repos    map[string]repo
+	ciStatus *CircleCi
+	window   fyne.Window
+	icon     *fyne.StaticResource
+	success  *fyne.StaticResource
+	running  *fyne.StaticResource
+	failed   *fyne.StaticResource
+
+	update chan int
+	app    fyne.App
+	modal  modal
 }
 
-var appl = App{}
+type repo struct {
+	repolabel  *widget.Hyperlink
+	q0Button   *widget.Button
+	q1Button   *widget.Button
+	prodButton *widget.Button
+}
+
+type modal struct {
+	modal     *widget.PopUp
+	repolabel *widget.Label
+	jaButton  *widget.Button
+	neiButton *widget.Button
+}
+
+func (ci *ciStatusLayout) Layout([]fyne.CanvasObject, fyne.Size) {
+	for i, project := range ci.ciStatus.projects {
+		u, _ := url.Parse(project.url)
+		ci.repos[i].repolabel.Text = fmt.Sprintf("%s\n%s", project.reponame, project.branch)
+		ci.repos[i].repolabel.URL = u
+
+		ci.repos[i].q0Button.Text = "Q0"
+		ci.repos[i].q1Button.Text = "Q1"
+		ci.repos[i].prodButton.Text = "Prod (master)"
+
+		if project.status == "success" {
+			if ci.repos[i].q0Button.Icon != ci.success {
+				ci.repos[i].q0Button.Enable()
+				ci.repos[i].q1Button.Enable()
+				ci.repos[i].q0Button.Icon = ci.success
+				ci.repos[i].q1Button.Icon = ci.success
+				widget.Refresh(ci.repos[i].q0Button)
+				widget.Refresh(ci.repos[i].q1Button)
+			}
+		}
+
+		if project.status == "running" {
+			if ci.repos[i].q0Button.Icon != ci.running {
+				ci.repos[i].q0Button.Disable()
+				ci.repos[i].q1Button.Disable()
+				ci.repos[i].q0Button.Icon = ci.running
+				ci.repos[i].q1Button.Icon = ci.running
+				widget.Refresh(ci.repos[i].q0Button)
+				widget.Refresh(ci.repos[i].q1Button)
+			}
+		}
+
+		if project.status == "failed" {
+			if ci.repos[i].q0Button.Icon != ci.failed {
+				ci.repos[i].q0Button.Disable()
+				ci.repos[i].q1Button.Disable()
+				ci.repos[i].q0Button.Icon = ci.failed
+				ci.repos[i].q1Button.Icon = ci.failed
+				widget.Refresh(ci.repos[i].q0Button)
+				widget.Refresh(ci.repos[i].q1Button)
+			}
+		}
+		if project.masterStatus == "success" {
+			if ci.repos[i].prodButton.Icon != ci.success {
+				ci.repos[i].prodButton.Enable()
+				ci.repos[i].prodButton.Icon = ci.success
+				widget.Refresh(ci.repos[i].prodButton)
+			}
+		}
+
+		if project.masterStatus == "running" {
+			if ci.repos[i].prodButton.Icon != ci.running {
+				ci.repos[i].prodButton.Disable()
+				ci.repos[i].prodButton.Icon = ci.running
+				widget.Refresh(ci.repos[i].prodButton)
+			}
+		}
+
+		if project.masterStatus == "failed" {
+			if ci.repos[i].prodButton.Icon != ci.failed {
+				ci.repos[i].prodButton.Disable()
+				ci.repos[i].prodButton.Icon = ci.failed
+				widget.Refresh(ci.repos[i].prodButton)
+			}
+		}
+
+		//ci.buttons[3*1].Icon =
+
+		ci.repos[i].q0Button.OnTapped = buttonFunc(ci, project.reponame, "q0")
+		ci.repos[i].q1Button.OnTapped = buttonFunc(ci, project.reponame, "q1")
+		ci.repos[i].prodButton.OnTapped = buttonFuncProd(ci, project.reponame)
+
+	}
+}
+
+func (ci ciStatusLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	container := fyne.NewContainerWithLayout(layout.NewGridLayout(2), objects[0], objects[1])
+	return container.Size()
+}
+
+func (ci *ciStatusLayout) render() *fyne.Container {
+	ci.repos = make(map[string]repo)
+	for k, project := range ci.ciStatus.projects {
+		u, _ := url.Parse(project.url)
+		hyperlink := widget.NewHyperlink(fmt.Sprintf("%s\n%s", project.reponame, project.branch), u)
+		ci.repos[k] = repo{
+			repolabel:  hyperlink,
+			q0Button:   widget.NewButtonWithIcon("", ci.icon, nil),
+			q1Button:   widget.NewButtonWithIcon("", ci.icon, nil),
+			prodButton: widget.NewButtonWithIcon("", ci.icon, nil),
+		}
+	}
+	var repolabels []fyne.CanvasObject
+	var buttons []fyne.CanvasObject
+	for _, value := range ci.repos {
+		repolabels = append(repolabels, value.repolabel)
+		buttons = append(buttons, value.q0Button)
+		buttons = append(buttons, value.q1Button)
+		buttons = append(buttons, value.prodButton)
+	}
+	ci.modal = modal{
+		repolabel: widget.NewLabel("Deploy til prod"),
+		jaButton:  widget.NewButton("Ja", nil),
+		neiButton: widget.NewButton("Nei", nil),
+	}
+
+	ci.modal.modal = widget.NewModalPopUp(widget.NewGroup("Deploy til prod", ci.modal.jaButton,
+		ci.modal.neiButton,
+	), ci.window.Canvas())
+	ci.modal.modal.Hide()
+	container := fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+		fyne.NewContainerWithLayout(layout.NewGridLayout(1), repolabels...),
+		fyne.NewContainerWithLayout(layout.NewGridLayout(3), buttons...))
+
+	container.Layout = ci
+	ci.canvas = container
+
+	return container
+}
+
+func (ci *ciStatusLayout) animate(canvas fyne.Canvas) {
+	tick := time.NewTicker(time.Second * 5)
+	tick5s := time.NewTicker(time.Second * 5)
+	go func() {
+		for {
+			ci.Layout(nil, canvas.Size().Subtract(fyne.NewSize(theme.Padding()*2, theme.Padding()*2)))
+			canvas.Refresh(ci.canvas)
+			<-tick.C
+		}
+	}()
+	go func() {
+		for {
+			ci.update <- 1
+			<-tick5s.C
+		}
+	}()
+}
+
+func buttonFunc(ci *ciStatusLayout, reponame, miljo string) func() {
+	return func() {
+		ciStatus := ci.ciStatus
+		p := ciStatus.projects[reponame]
+		u, _ := url.Parse(fmt.Sprintf("https://circleci.com/gh/navikt/%s/%d", reponame, p.buildNum+1))
+		m := make(map[string]string)
+		m["VERSION"] = p.vcsRevision
+		m["CIRCLE_JOB"] = "deploy_miljo"
+		m["MILJO"] = miljo
+		_, e := ciStatus.client.ParameterizedBuild("navikt", reponame, p.branch, m)
+		if e != nil {
+			log.Fatal(e)
+		}
+		e = ci.app.OpenURL(u)
+		if e != nil {
+			log.Fatal(e)
+		}
+	}
+}
+
+func buttonFuncProd(ci *ciStatusLayout, reponame string) func() {
+	return func() {
+		ci.modal.modal.Show()
+		ci.modal.neiButton.OnTapped = func() {
+			ci.modal.modal.Hide()
+		}
+		ci.modal.jaButton.OnTapped = func() {
+			ciStatus := ci.ciStatus
+			p := ciStatus.projects[reponame]
+			u, _ := url.Parse(fmt.Sprintf("https://circleci.com/gh/navikt/%s/%d", reponame, p.buildNum+1))
+			m := make(map[string]string)
+			m["VERSION"] = p.vcsRevision
+			m["CIRCLE_JOB"] = "deploy_prod"
+			_, e := ciStatus.client.ParameterizedBuild("navikt", reponame, "master", m)
+			if e != nil {
+				log.Fatal(e)
+			}
+			e = ci.app.OpenURL(u)
+			if e != nil {
+				log.Fatal(e)
+			}
+			ci.modal.modal.Hide()
+		}
+
+	}
+}
 
 func main() {
-	appl.a = app.New()
-
-	appl.a.SetIcon(fyne.NewStaticResource("icon", decode(icon)))
-
-	appl.w = appl.a.NewWindow("CircleCi deploy app")
-	appl.w.SetIcon(fyne.NewStaticResource("icon", decode(icon)))
-	appl.w.SetFixedSize(true)
-	go updateProjects()
-	appl.w.ShowAndRun()
-}
-
-type Config struct {
-	Citoken string
-}
-
-func readConfig() Config {
-	homeDir, e := os.UserHomeDir()
-	if e != nil {
-		log.Fatal(e)
-	}
-	var config = Config{}
-	bytes, e := ioutil.ReadFile(homeDir + "/.cistatus.json")
-	if e != nil {
-		confb, e := json.Marshal(config)
-		if e != nil {
-			log.Fatal(e)
-		}
-		e = ioutil.WriteFile(homeDir+"/.cistatus.json", confb, 0666)
-		if e != nil {
-			log.Fatal(e)
-		}
-		log.Println("Add Citoken in " + homeDir + "/.cistatus.json")
-		os.Exit(-1)
-	}
-
-	e = json.Unmarshal(bytes, &config)
-	if e != nil {
-		log.Println("Add Citoken in " + homeDir + "/.cistatus.json")
-		os.Exit(-1)
-	}
-	return config
+	application := app.New()
+	ci := &ciStatusLayout{}
+	ci.app = application
+	ci.window = application.NewWindow("CircleCi deploy app")
+	ci.icon = fyne.NewStaticResource("icon", decode(icon))
+	ci.success = fyne.NewStaticResource("success", decode(success))
+	ci.running = fyne.NewStaticResource("running", decode(running))
+	ci.failed = fyne.NewStaticResource("failed", decode(failed))
+	application.SetIcon(ci.icon)
+	ci.ciStatus = &CircleCi{}
+	ci.update = make(chan int)
+	go ci.ciStatus.update(ci.update)
+	ci.update <- 1
+	canvas := ci.render()
+	go ci.animate(ci.window.Canvas())
+	ci.window.SetContent(canvas)
+	ci.window.ShowAndRun()
 }
 
 func decode(str string) []byte {
@@ -75,121 +253,6 @@ func decode(str string) []byte {
 		fmt.Println("error:", err)
 	}
 	return data
-}
-func updateProjects() {
-
-	successIcon := fyne.NewStaticResource("icon", decode(success))
-	runningIcon := fyne.NewStaticResource("icon", decode(running))
-	failed := fyne.NewStaticResource("icon", decode(failed))
-	for {
-		client := &circleci.Client{Token: readConfig().Citoken}
-		projects, e := client.ListProjects()
-		appl.projects = projects
-		for e != nil {
-			time.Sleep(10 * time.Second)
-			appl.projects, e = client.ListProjects()
-		}
-
-		var containers []fyne.CanvasObject
-		var repolabels []fyne.CanvasObject
-		for _, repo := range projects {
-			repoName := repo.Reponame
-			status, _ := client.ListRecentBuildsForProject("navikt", repoName, "", "", 1, 0)
-			u, _ := url.Parse(fmt.Sprintf("https://circleci.com/gh/navikt/%s/%d", repoName, status[0].BuildNum))
-			branch := status[0].Branch
-			repoLabel := widget.NewHyperlink(fmt.Sprintf("%s %s", repoName, branch), u)
-
-			repoLabel.TextStyle = fyne.TextStyle{Monospace: true}
-			prodStatus, _ := client.ListRecentBuildsForProject("navikt", repoName, "master", "", 1, 0)
-			prodbutton := getProdButton(client, repoName)
-
-			if prodStatus[0].Status == "success" {
-				prodbutton.Enable()
-			} else {
-				prodbutton.Disable()
-			}
-
-			q0Button := widget.NewButton("Q0", buttonFunc(client, repoName, "q0"))
-			q1Button := widget.NewButton("Q1", buttonFunc(client, repoName, "q1"))
-
-			if status[0].Status == "success" {
-				q0Button = widget.NewButtonWithIcon("Q0", successIcon, buttonFunc(client, repoName, "q0"))
-				q0Button.Enable()
-				q1Button = widget.NewButtonWithIcon("Q1", successIcon, buttonFunc(client, repoName, "q1"))
-				q1Button.Enable()
-			} else {
-				if status[0].Status == "running" {
-					q0Button = widget.NewButtonWithIcon("Q0", runningIcon, buttonFunc(client, repoName, "q0"))
-					q1Button = widget.NewButtonWithIcon("Q1", runningIcon, buttonFunc(client, repoName, "q1"))
-				}
-				if status[0].Status == "failed" {
-					q0Button = widget.NewButtonWithIcon("Q0", failed, buttonFunc(client, repoName, "q0"))
-					q1Button = widget.NewButtonWithIcon("Q1", failed, buttonFunc(client, repoName, "q1"))
-				}
-
-				q0Button.Disable()
-				q1Button.Disable()
-			}
-			repolabels = append(repolabels, repoLabel)
-			containers = append(containers,
-				q0Button,
-				q1Button,
-				prodbutton,
-			)
-		}
-
-		appl.w.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(2), fyne.NewContainerWithLayout(layout.NewGridLayout(1), repolabels...), fyne.NewContainerWithLayout(layout.NewGridLayout(3), containers...)))
-
-		time.Sleep(10 * time.Second)
-	}
-}
-
-func getProdButton(client *circleci.Client, repoName string) *widget.Button {
-	return widget.NewButton("Prod (master)", func() {
-		neiButton := widget.NewButton("Nei", func() {
-			appl.modal.Hide()
-		})
-		jaButton := widget.NewButton("Ja", func() {
-			status, _ := client.ListRecentBuildsForProject("navikt", repoName, "master", "", 1, 0)
-
-			m := make(map[string]string)
-			m["VERSION"] = status[0].VcsRevision
-			m["CIRCLE_JOB"] = "deploy_prod"
-			_, e := client.ParameterizedBuild("navikt", repoName, "master", m)
-			if e != nil {
-				log.Fatal(e)
-			}
-			u, _ := url.Parse(fmt.Sprintf("https://circleci.com/gh/navikt/%s/%d", repoName, status[0].BuildNum+1))
-			e = appl.a.OpenURL(u)
-			if e != nil {
-				log.Fatal(e)
-			}
-			appl.modal.Hide()
-		})
-
-		jaButton.Style = widget.PrimaryButton
-
-		appl.modal = widget.NewModalPopUp(widget.NewGroup("Deploy til prod", jaButton,
-			neiButton,
-		), appl.w.Canvas())
-
-	})
-}
-
-func buttonFunc(client *circleci.Client, reponame, miljo string) func() {
-	return func() {
-		status, _ := client.ListRecentBuildsForProject("navikt", reponame, "", "", -1, 0)
-		u, _ := url.Parse(fmt.Sprintf("https://circleci.com/gh/navikt/%s/%d", reponame, status[0].BuildNum+1))
-		m := make(map[string]string)
-		m["VERSION"] = status[0].VcsRevision
-		m["CIRCLE_JOB"] = "deploy_miljo"
-		m["MILJO"] = miljo
-		client.ParameterizedBuild("navikt", reponame, status[0].Branch, m)
-		e := appl.a.OpenURL(u)
-		if e != nil {
-			log.Fatal(e)
-		}
-	}
 }
 
 var icon = "iVBORw0KGgoAAAANSUhEUgAAAFUAAABVCAIAAAC3lz8NAAAN8UlEQVR4nNRcCVgTWbauVCr7HkLYl+7WBkEEexHUdtrd5zg943vz3LfWtnu0tZ8zPttBoVltbD97ZpxpW8d9ax0Vh0FEHTdUFBEUoUdlVyGsgUCAhOypmk8DISmSSiWpZOz/4/uousu556976+bcc88tCEEQwGPQ6XSP/1XxsLSk7GHp8/p6vV6HpxYIgoFBwXFj33k/fvw7743jcrme05DkCf4Igjwqe3AhN6e46I5Go3FHFAiC0aNj5nw0d8r0mXQ6nTgdB0Awf7ValZtz7kJuTktzE4FiAQBgszkzZ8+Zt2BRYFAwgWIJ428wGC7n5x05tK9LJiNEoE1AEDT7F79c+clvfEQiQgQSw7/ozu3vdv2hrbWFCJUcg8FgLlq6YsnyjyEIclOUu/z7+/v37t51ITfHTT1cwJtvjUhKzRwx8m13hLjFv6K8LCs9RSptd0cDd0ClUj9du37egsUkEsk1Ca7zz8/L/dPO7QaDwbXqBGLy1OlbUzJoNJoLdV3hD8Pwgb9+f+rEURfa8xCiRsdk7fijQCh0tqLT/PV6XWpSYtGd28625GkEBAb94c/fBwWHOFXLOf4GgyE16fd3C285r543IPbz+8ueAwGBQfirgPiLwjCclZH62pIHAKBDKt3w+WfS9jb8VfDyRxDkm23pN67901XdvASptH3ThnU9PXKc5fHyP3ni6JXL+W4o5j1IJI3JiZv0ej2ewrj4l5YUH9q3x23FvIfHP1bs+W4XnpKO+UsaG9KSE2EYJkIx7yEn+/Tl/DyHxRzM/0ajce3qFTXVVYTq5iVQKNSDx06Gv/EmRhkH/X/8yMGfKHmTqbIjKwN75GLxr6+r/eHYYQ8o5j1UPnmcffoURgG7/GEY3p6Z9jqY927i0P49GM4Yu/wvX8yrr6vxmFbeg1ar3b93t71c2/y1Wu3RQ/s9qZVXcfvmjconj21m2eaffeZUh1TqYa28BwRB9u35zmaWDf5qtepvJ455XiuvoqK8rOJR2fB0G/yvXr6kVCpca4Yv4P/3r+e57I3xKP6efXp4og3/YW5OtmsNCAXCr9IzA4OCSCRSzrmzrgnxHO4W3pK2t/n5B1gmovv/0cMHz5/VuyBd6CNKydwWGPRy7T1vwcLYse+4py3xgGE4Py8XlYjmfyn/vAuiRSLftMxtAQGBA0JB8Iv/+63I19dVVT2FSxfOo8xBK/4Gg6G46K6zQkUi35SMTD8/f8tEDpe7JSmFzWa7oS3xkMk6qyqfWKZY8S97UOLszOfn75e+LQtF3oTgkJDNW5Jdc8t6DncLrTyXVvwLb990SpavWPxVaibGOI+IjNySnEKnM5zX01MovFVgeWvFv6T4Hn5B/v4BmV9v9xWLsYuNiorevGXr6/MImpskrS3N5tsh/lJpe2cHXptP6CNKSkkTCH3wFI4eHZORtV3oQ8yOpft4amELD/G3ZyEPB4/PS0lLF/v54W8yLCw88+vtYeFYrgivobrqqfnagv/TJ3gqs9ns5NQMp3zsJoh8fbdt/2bK1OnOViQclkyH+NdUVTqsSaVSN29NDg0Nc61hKpW6Zt36DRs38Xg81yQQgvraGqPRaLoe4t/cLMGu9tKq2fC7iIhIN5ufMPGDXbv3TJs58z+1TNDpdLLODtM1aE7q7urCrrZ85apxCeMJ0YDJZH32m8+3JKeEhIYSItBZtA/uEQ2sf9rbWrEdwdNnzJr9818Qq0Rs3NgxsXEl94vP/O1ka4uXYkdMkLa1AXGAFX+M0iNGjFy5erUn9CCRSAnjJ4yLTygqLLzyz0t1dbWeaGU42tqs+79HbnfDjMFg/O7LzRBE8Zw2IAhOmjx50uTJTRJJwY1rdwpvK/r6PNfcK77dposB/mq12l7R+QsXi0ReWsmFhIauWPnJkmUrqqoq/1Ve/mNFeWNjgyca0mq1pgu7/Gk0WnBIqEGvnzX7557QAAMQBMXEjImJGbNk+YpueXdNVVVjQ0Nzk0QiaeiQduAPWKBQKGw2W25raGu1A1GZA/w1Giv+/v4BSSlpTll4HoJQIBw/YeL4CRNNtzqdTi7v7u3tUfQpFH19fX29lk+DwWBweTwul8vhvPrjcshkcnVl5c4dWUql0lKsVmPN3zweTFi0dJmb5HvkPbduXgdB8uQpU7nuWTutLS3F94rEYvEHP/uQSqX6+fnbXG7bQ2RU1PyFiw8ftHLna1D9j7JEwsLD3dEYhuETx49I29tN2q9Zt95lUf1K5eGD+01BxDq9ftr0GS4IeXuYzUYiDRg+A//IZCtHKMW92f5ZfZ2J/CuzsqnLkWWFgcqnT80R1EV3CjUarQtChoeJQmSy6WLwMYDWjkD3DNMWiwU2AAByN/jLuoaiiXU6XQdBsZZkFH/zvQkk9x6AUmE12UAU10cTGbTSxOWNCbTYwRFhe/yDoFv8LYOzQTLZz9/1qRTlrhcSFPZt5js4/slW4x9xb2U2cuTb5rXdqFFRDAbTZVERkVFMFst0zRfwxQRZYmTIk+Nf5Ov70a/minx9Y8bE/nLuXHdE0em0pctWhIaFBQeHzF+4GLTW02WY+UKoexPc5A8AwLj4hHHxCW4KMSE0LOyzNZ8TIsoMCPX+o34hXs8NTAJh/r0boI0+WkTy4KEwFJSy55o+KQAgZCqLLQyjMPleaNTMd+AxsNkcq3zEe/1PhmgUOpfCFBp1annLY4NW5YVGudwBk3yAP4djdcZO2uFECLGbYPCDeIHRPP9IQUgsACAquQM3pAvoknWiUsx8B/ufY9X/586c8X7kF5nCoDJ4eg3Bng+j0Zifh97U5gyeqRx4/1Hj/8eK8sQvN46JHdsl63w/PuGDST8jViebQBDYoFOTKcTsl966WdDcJOFwuPeK7jS8eIHKNff/AH8Ol4Mq0SSRNEleDkUWh+0F/giCKDvqYYOWJXJr6WnGwwelD0ru28vlovqfTmdQKBSbMeNVT58SotBwILCRBL60OwwahVL2XKfqoXPFDK4Ta3u7khGkGnM7Bz3+SSSSn39Ac5ONuae1paWzo8PhPq/zKsJdL4qBV+tw2KAnkUCWKJwpIGY74PmzegwPKgiCZjpDZj/GyaHSUrsDyWW8IvwWlSGgMn044hE+b8SzhGFE2V33i4sxcv0DAikUqukaH3/7L5I7YPACuAGjuP4RDH4QCFEJlPygtAQjN8Ri/3KIf7B9/rXV1T+hcNC6ulrsk8jBIUNMh/iPfDvCXgUYhgtuXCdOQ8/i+hUHh7QsjwwP8Y8YFUWx76i5VXDdMLhnjB8GlUrf0+NsLUvoOmWIM+329yvvOQphGxM71nw9xJ9Go2EMAblcfvtmAX49TFC/aOh5WAZr7G4uYUPT2trz6JG+G+9htldh+/k6HdZXNnh8vuVMZ+X2GR0Ti1Ez59xZZ4cA840wAEF6yitgg9NjB9bqlLW1EJtFEeGKMgIAQKVSX77o4JBezJg4y18ZK/4Jg9ssNiHr7CwsuIFTFRPITBYnKsrQp+grLwecOUEGG4y95eWwTs8dHYP/R/FS/nnUPs9wxI+fYHlrxX/su+/z+QKMyidPnlAqnPPA0oMCOaMidXJ59/37BoUD5UwwqlQ9pSX63l5udDTEw/vxF5msM+88OrwXBRAEJ304xSoFlT1x0ocY9ZUKRfYZG1Hk2GCEhvLjYo1qrbykRFlTC2vtvp+I0ahqaOguvm9Ua3ixsfSgQPytHD18SOvoWzNx77wrEFidkUdvjEybOeviBaynePXK5ffjE0bHxODXDAAAqljsM5GjqKlVNTaqGhupAgHFxwdis8h0OkACYIMBVmt1XTKdrAvW6Sh8Pjc6msxm4ZdfUnwPY7VjxrTps1ApNs4/frJ8MfbJJ4HQ59s//dm12GaDQqlubtbJOo0q9I8CCYKoAj4jNJTqpJO/u0v25cbfOnzzuTxe9j8u0hlWkag2zj/MX7Q4KyMVQ5C8u2vv7r/8/+ZE9K4ZDkAcNmdUJABEGtUaWK02vhqxJAgCaVSIwyE5L9Bg0O/647cOyQMA8D//uwBF3vb5l2kz/svhBvPDB6WnT/3gpKpWIDPoFKGAHhhADwygiX0pPJ4L5AEAOHTwQE11tcNidDp97q/nDU+30SQEQWvWb3Ao8fw/cm5cvYpbT4/g79lnC67h0mHpilWomc8E2498yrQZ7743zqHQA/v3/gcfwcX8vLOYZ1vNCAkNW7B4mc0s2/xJJNLWlHSUU3g4EAQ5eOCvBTeu4VGCWOTl5hw/gutsMplM3pqSQaXaXl/bfeVEvuLfJ6U4tL1gGN635/tjRw567QMBMAwfPrD/5InjOMt/unZ9VPRoe7nktLQ0e3lh4W+AIFhe9tBhG3W1tU1Nkti4OHuPmSioVOqd33ztcIVnxuw5H63FnMscTLnLV65esvxjPC2p9bAaoVCpHjztQ6Mzwt9864vE9ITJuKKAJn04ZVNiMnYZXN//ObB39w/Hj9jLDXnzrVVfbJ6a8J7pVq3qV/T1wrDTCz4MkMlkNofHYA5ZhCcvXt+9PZVqtGvwTpk2Iyk1E8OjYQKu78d9unZ9QFDwrm93DP+AZRfTf86vVpnJv7T2mSw6ndHfr1D1K92fFECQzGKzmSwOaiaq69VV+o/zU0j8FBIQsWqFRCItWfbx6jXr8Cwcnfj+04vnz9K/2vLi+TPTrRZiNAoilTQejQIV7dwYEYQOckEQRK3qV6v79ZgOCXugUGkMBpPBZA2nodbpY9dntcn7Xr4UBnWovJajHfCR8Pj8xKTUCR/g3bD5dwAAAP//COcpfed+2uwAAAAASUVORK5CYII="
