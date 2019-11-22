d3460f8ff1619088ba3c699f2eed4d39c1847f68
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2019-11-22 14:21:45 +0100

    bash to sh.

--- bin/create-artifact-version.sh
+++ bin/create-artifact-version.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
+if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = "true" ]; then
     GIT_COMMIT_HASH=$(git log -n 1 --pretty=format:'%h')
     GIT_COMMIT_DATE=$(git log -1 --pretty='%ad' --date=format:'%Y%m%d.%H%M')
     echo "1.0_${GIT_COMMIT_DATE}_${GIT_COMMIT_HASH}"
